9ca6492c179c5f3af2087e5d02a30c252525316d
mennarihan5 <menna1986@live.com>
2024-01-20 22:18:33 +0200

    Edited Css and JS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let container = document.querySelector(".main-container");
 let userInput = document.getElementById("user-input");
 let iconUp = document.querySelector(".up-down-icon-up");
 let iconDown = document.querySelector(".up-down-icon-down");
@@ -5,98 +6,69 @@ let messagesBody = document.querySelector(".chatbox-msgs");
 let chatHeader = document.querySelector(".chatbox-header");
 let showIcon = document.querySelector(".icon");
 
-
-
-
-// BOT MESSAGES
-const botMessages = {
-    "hi": "Hi, How can I help you?",
-    "hello": "Hello, How can I help you?",
-    "hey": "Hey, How can I help you?",
-    "good morning": "Good morning, How can I help you?",
-    "good evening": "Good evening, How can I help you?",
-    "good afternoon": "Good afternoon, How can I help you?",
-    "how are you?": "All's good, and you?",
-    "help me": "Sure, How can I help you?",
-    "explain": "Sure, What would you like me to explain?",
-    "i don't get it": "Sure, Let me explain again",
-    "bye": "Bye, See you soon..."
-}
-const connectHuman = ["connect with customer support agent", "human", "i need a someone to talk to", "conect me with a human"];
-
-
 // BOT WELCOME MESSAGE
-setTimeout(() => {
-    messagesBody.innerHTML += `<div class="chatbox-bot-msg">
+function displayBotMsg(message) {
+    messagesBody.innerHTML += `<div class="chatbox-bot-msg-wrapper">
     <div class="chatbox-bot-img">
-        <img src="./images/Frame 31291 (1).png" alt="">
+    <img src="./images/Frame 31291 (1).png" alt="">
     </div>
-    <p class="chatbox-bot-msg-txt">👋 Hi there! How can I help?</p>
+    <div class="chatbox-bot-msg">
+    <p class="chatbox-bot-msg-txt">${message}</p>
+    </div>
     </div>`   
+}
+
+setTimeout(() => {
+    displayBotMsg("👋 Hi there! How can I help?");
 }, 950);
 
-
 // GET USER INPUT ON PRESSING ENTER & CLEARING IT
-userInput.addEventListener("keypress", (event) => {
-    
-    let inputValue = userInput.value.toLowerCase();
+userInput.addEventListener("keypress", handleUserInput);
+
+// HANDLE USER INPUT FUNCTION
+function handleUserInput(event) {
     if (event.key === "Enter") {
-        // DISPLAY USER INPUT VALUE
-        messagesBody.innerHTML += inputValue;
+         // DISPLAY USER INPUT VALUE
+         let inputValue = userInput.value.toLowerCase();
+         let userMsg = document.createElement("div");
+         userMsg.classList.add("user-msg");
+         messagesBody.appendChild(userMsg);
+         userMsg.innerText = inputValue;
+
         // DISPLAY BOT REPLY
         if (connectHuman.some(el => inputValue.includes(el))) {
-            
-            messagesBody.innerHTML += `<div class="chatbox-bot-msg">
-            <div class="chatbox-bot-img">
-                <img src="./images/Frame 31291 (1).png" alt="">
-            </div>
-            <p class="chatbox-bot-msg-txt">Will connect you to a customer support agent shortly...</p>
-            </div>`   
+            displayBotMsg("Let me connect you to a customer support agent shortly...");
 
+            // CONNECT USER WITH HANNAH
             setTimeout(() => {
-            messagesBody.innerHTML +=`<div class="chatbox-hannah-msg">
+            messagesBody.innerHTML +=` <div class="chatbox-hannah-msg-wrapper">
             <div class="chatbox-hannah-img">
                 <img src="./images/hannah.png" alt="">
             </div>
-            <p class="chatbox-hannah-msg-txt">
-                Hi there! I’m Hannah. <br>
-                How can I help you?
-            </p>
+            <div class="chatbox-hannah-msg-txt-wrapper">
+                <p class="chatbox-hannah-msg-txt">Hi there! I’m Hannah. <br>
+                How can I help you?</p>
+            </div>
             </div>`
             },2000)
 
         } else if (inputValue in botMessages) {
             let botMsg = botMessages[inputValue];
-     
-            messagesBody.innerHTML += `<div class="chatbox-bot-msg">
-            <div class="chatbox-bot-img">
-                <img src="./images/Frame 31291 (1).png" alt="">
-            </div>
-            <p class="chatbox-bot-msg-txt">${botMsg}</p>
-            </div>`
-
+            displayBotMsg(botMsg);
         } else {
-            messagesBody.innerHTML += `<div class="chatbox-bot-msg">
-            <div class="chatbox-bot-img">
-                <img src="./images/Frame 31291 (1).png" alt="">
-            </div>
-            <p class="chatbox-bot-msg-txt">Please enter something else...</p>
-            </div>`   
+            displayBotMsg("Please enter something else...");
         }
         
-        // CLEAR INPUT VALUE 
-        userInput.value = "";
-
         // CHANGE HEADER IMAGE AND NAME
         function changeAgent() {
             if (connectHuman.some((el) => inputValue.includes(el))) {
                 chatHeader.innerHTML = `<div class="header-left-icon">
                 <i class="fa-solid fa-chevron-left"></i>
                 </div>
-                <div class="header-agent-img">
+                <div class="header-agent-img hannah">
                     <img src="./images/hannah.png" alt="">
                 </div>
-                <div class="header-agent-name-wrapper">
+                <div class="header-agent-name-wrapper-hannah">
                 <p class="header-agent-name">Hannah</p>
                 <p class="header-agent-status">Active</p>
                 </div>`
@@ -105,44 +77,57 @@ userInput.addEventListener("keypress", (event) => {
                 chatHeader.innerHTML = `<div class="header-left-icon">
                 <i class="fa-solid fa-chevron-left"></i>
                 </div>
-                <div class="header-agent-img">
+                <div class="header-agent-img bot">
                     <img src="./images/Frame 31291 (1).png" alt="">
                 </div>
-                <div class="header-agent-name-wrapper">
+                <div class="header-agent-name-wrapper-bot">
                     <p class="header-agent-name">Bot</p>
                     <p class="header-agent-status">Active</p>
                 </div>`
             }
         }
 
-        setTimeout(() => {
-            changeAgent();
-        }, 2000);
+    // CLEAR INPUT VALUE 
+    userInput.value = "";
 
-            } 
-        })
+    // CHANGE AGENT AFTER 2S
+    setTimeout(() => {
+        changeAgent();
+    }, 2000);
 
-// CLEARING USER INPUT ON REFRESH
+    }
+}
+
+// CLEARING USER INPUT ON RELOAD
 window.onload = clearInput;
 
 function clearInput() {
     userInput.value = "";
 }
 
-// SCROLL WINDOW TO UP
+// SHOW CHATBOX
 iconUp.addEventListener("click", () => {
-    let windowHeight = window.innerHeight;
+    container.style.display = container.style.display === "none" ? "block" : "none";
+    iconUp.style.display = iconUp.style.display === "block" ? "none" : "block";
+    iconDown.style.display = iconDown.style.display === "none" ? "block" : "none";
+}
+);
 
-    window.scrollBy(0, windowHeight);
-})
-
-// SCROLL WINDOW DOWN
+// HIDE CHATBOX
 iconDown.addEventListener("click", () => {
-    
-    window.scrollTo(0, 0);
-})
+    container.style.display = container.style.display === "block" ? "none" : "block";
+    iconDown.style.display = iconDown.style.display === "none" ? "block" : "none";
+    iconUp.style.display = iconUp.style.display === "none" ? "block" : "none";  
+}
+);
 
-// SHOW & HIDE ICON
-showIcon.addEventListener("click", () => {
-    showIcon.classList.toggle("hide");
-})
+
+
+
+
+
+
+
+
+
+
---
+++ messages.js
@@ -0,0 +1,15 @@
+// BOT MESSAGES
+const botMessages = {
+    "hi": "Hi, How can I help you?",
+    "hello": "Hello, How can I help you?",
+    "hey": "Hey, How can I help you?",
+    "good morning": "Good morning, How can I help you?",
+    "good evening": "Good evening, How can I help you?",
+    "good afternoon": "Good afternoon, How can I help you?",
+    "how are you?": "All's good, and you?",
+    "help me": "Sure, How can I help you?",
+    "explain": "Sure, What would you like me to explain?",
+    "i don't get it": "Sure, Let me explain again",
+    "bye": "Bye, See you soon..."
+}
+const connectHuman = ["connect with customer support agent", "human", "i need a someone to talk to", "conect me with a human"];
